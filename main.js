9e27c8f63830be3bcacb1a4ca722eb58c7af0f6e
metlebh <121571075+metlebh@users.noreply.github.com>
2023-04-20 18:28:58 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// function FirstLater(string) {
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+//   }
+  
+//   const string1 = "hello my name is Matlab";
+//   const string2 = FirstLater(string1);
+//   console.log(string2);  
+  
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// function Reverse(string) {
+//     let Reversed = "";
+//     for (let i = string.length - 1; i >= 0; i--) {
+//       Reversed += string[i];
+//     }
+//     return Reversed;
+//   }
+  
+//   let str = "hello my name is Matlab";
+//   let Reversed = Reverse(str);
+//   console.log(Reversed);  
+  
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
